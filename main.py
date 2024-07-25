7cc5da40044c0ac0785502f4947da9f86996ae68
Ishmael <70352247+Ishmael-sikhikhi@users.noreply.github.com>
2024-07-25 15:22:15 +0200

    Quiz game with python

---
+++ main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import  QuizBrain
+
+questions_bank = []
+for question in question_data:
+    questions_text = question["text"]
+    questions_answer = question["answer"]
+    new_question = Question(questions_text, questions_answer)
+    questions_bank.append(new_question)
+
+quiz = QuizBrain(questions_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print("Your final score is " + str(quiz.score))
